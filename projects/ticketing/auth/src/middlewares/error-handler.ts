39afa6b6107fe9dd27b7fa9fa65cbe3755d2f61e
LeeTrent <lee.trent.1@gmail.com>
2020-10-04 11:19:14 -0500

    Convereted errors into http responses

--- projects/ticketing/auth/src/middlewares/error-handler.ts
+++ projects/ticketing/auth/src/middlewares/error-handler.ts
@@ -3,21 +3,26 @@ import { DatabaseConnectionError } from '../errors/database-connection-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 
 export const errorHandler = (
-    error: Error, 
+    err: Error, 
     request: Request,
     response: Response,
     next : NextFunction
 ) => {
 
-    if ( error instanceof RequestValidationError ) {
+    if ( err instanceof RequestValidationError ) {
         console.log("[error-handler.ts][errorHandler] => RequestValidationError");
+        const formattedErrors = err.errors.map(error => {
+            return { message: error.msg, field: error.param };
+        });
+        return response.status(400).send({ errors: formattedErrors });
     }   
     
-    if ( error instanceof DatabaseConnectionError ) {
+    if ( err instanceof DatabaseConnectionError ) {
         console.log("[error-handler.ts][errorHandler] => DatabaseConnectionError");
+        return response.status(500).send({ errors: [ { message: err.reason } ] });
     }     
     
     response.status(400).send({
-        message: error.message
+        errors: [ { message: 'Unexpected server-side error encountered.'} ]
     });
 };
