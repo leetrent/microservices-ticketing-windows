4aad6e45519c7bcb8cc17f0e20d195f0730b3019
LeeTrent <lee.trent.1@gmail.com>
2020-10-09 11:03:57 -0500

    Implements password hashing

--- projects/ticketing/auth/src/models/user.ts
+++ projects/ticketing/auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 /////////////////////////////////////////////
 // new User:
@@ -38,6 +39,14 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+});
+
+userSchema.pre('save', async function(done) {
+  if ( this.isModified('password') ) {
+    const hashedPassword = await Password.toHash( this.get('password') );
+    this.set('password', hashedPassword);
+  }
+  done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
