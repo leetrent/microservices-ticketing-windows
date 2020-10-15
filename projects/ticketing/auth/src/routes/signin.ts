1f14b23269dd121e7504ad20f638ce11333db689
LeeTrent <lee.trent.1@gmail.com>
2020-10-15 08:41:17 -0500

    Moved validation logic to a newly created middleware component

--- projects/ticketing/auth/src/routes/signin.ts
+++ projects/ticketing/auth/src/routes/signin.ts
@@ -1,8 +1,16 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
-router.post('/api/users/signin', (request, response) => {
-    response.send("[signInRouter][HttpPost] =>")
+router.post('/api/users/signin', 
+[
+    body('email').isEmail().withMessage('Invalid email address'),
+    body('password').trim().notEmpty().withMessage('Password is required.')
+], 
+validateRequest,
+(request: Request, response: Response) => {
+    //response.send("[signInRouter][HttpPost] =>")
 });
 
 export { router as signInRouter };
--- projects/ticketing/auth/src/routes/signup.ts
+++ projects/ticketing/auth/src/routes/signup.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken'; 
+import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error'
 
 const router = express.Router();
@@ -11,11 +11,8 @@ router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Invalid email address'),
     body('password').trim().isLength({min: 4, max: 20}).withMessage("Password must be between 4 and 20 characters.")
 ],
+validateRequest,
 async (request: Request, response: Response ) => {
-    const errors = validationResult(request);
-    if ( !errors.isEmpty() ) {
-        throw new RequestValidationError(errors.array());  
-    }
     const { email, password} = request.body;
     const existingUser = await User.findOne({email});
     if (existingUser) {
