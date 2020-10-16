b664ef21db522265278c70084b87eb71ad32bf73
LeeTrent <lee.trent.1@gmail.com>
2020-10-16 07:24:41 -0500

    Completes signin implementation

--- projects/ticketing/auth/src/routes/signin.ts
+++ projects/ticketing/auth/src/routes/signin.ts
@@ -1,16 +1,36 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { User } from  '../models/user';
+import { Password } from '../services/password';
 import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
 
-const router = express.Router();
+const router = express.Router();    
 router.post('/api/users/signin', 
 [
     body('email').isEmail().withMessage('Invalid email address'),
     body('password').trim().notEmpty().withMessage('Password is required.')
 ], 
 validateRequest,
-(request: Request, response: Response) => {
-    //response.send("[signInRouter][HttpPost] =>")
+async (request: Request, response: Response) => {
+    const { email, password } = request.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+        throw new BadRequestError('Invalid login credentials');
+    }
+    const passwordsMatch = await Password.compare(existingUser.password, password);
+    if (!passwordsMatch) {
+        throw new BadRequestError('Invalid login credentials');
+    }
+    const userJwt = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+    }, process.env.JWT_KEY!);
+    request.session = {
+        jwt: userJwt
+    };
+    response.status(200).send(existingUser);
 });
 
 export { router as signInRouter };
