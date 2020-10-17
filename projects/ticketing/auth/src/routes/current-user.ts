8bfead6c80e46ac8c93f0c7f6fb59e16d9519ca2
LeeTrent <lee.trent.1@gmail.com>
2020-10-17 11:25:00 -0500

    Adds require-auth middleware

--- projects/ticketing/auth/src/routes/current-user.ts
+++ projects/ticketing/auth/src/routes/current-user.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-//import jwt from 'jsonwebtoken';
 import { currentUser } from '../middlewares/current-user';
-
 
 const router = express.Router();
 router.get('/api/users/currentuser', currentUser, (request, response) => {
