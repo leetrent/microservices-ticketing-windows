2927774a33b8f15b525b8c127a0246e7129e878f
LeeTrent <lee.trent.1@gmail.com>
2020-10-21 10:21:40 -0500

    Adds tests

--- projects/ticketing/auth/src/routes/__tests__/signup.test.ts
+++ projects/ticketing/auth/src/routes/__tests__/signup.test.ts
@@ -11,3 +11,85 @@ it('returns a 201 on successful signup', async() => {
     })
     .expect(201)
 });
+
+it('returns a 400 with an invalid email.', async() => {
+  return 
+    request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'bademailaddress,',
+      password: 'password'
+    })
+    .expect(400)
+});
+
+it('returns a 400 with an invalid password.', async() => {
+  return 
+    request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'bademailaddress,',
+      password: 'p'
+    })
+    .expect(400)
+});
+
+it('returns a 400 with missing email.', async() => {
+  return 
+    request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'password'
+    })
+    .expect(400)
+});
+
+it('returns a 400 with missing password.', async() => {
+  return 
+    request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+    })
+    .expect(400)
+});
+
+it('returns a 400 with missing email and password.', async() => {
+  return 
+    request(app)
+    .post('/api/users/signup')
+    .send({})
+    .expect(400)
+});
+
+it('returns a 400 with missing email and password (async).', async() => {
+  await 
+    request(app)
+    .post('/api/users/signup')
+    .send({email: 'test@test.com'})
+    .expect(400);
+  await 
+    request(app)
+    .post('/api/users/signup')
+    .send({password: 'password'})
+    .expect(400);
+});
+
+it('disallows duplicate emails', async() => {
+  await 
+    request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com,',
+      password: 'password'
+    })
+    .expect(201);
+  await 
+    request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com,',
+      password: 'password'
+    })
+    .expect(400);
+});
