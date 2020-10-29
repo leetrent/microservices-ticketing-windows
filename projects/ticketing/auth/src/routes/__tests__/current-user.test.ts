056d276f45491a043f9f79239853dde85fb34c5d
LeeTrent <lee.trent.1@gmail.com>
2020-10-29 09:52:34 -0500

    Completes current-user tests

--- projects/ticketing/auth/src/routes/__tests__/current-user.test.ts
+++ projects/ticketing/auth/src/routes/__tests__/current-user.test.ts
@@ -12,3 +12,13 @@ it('responds with details about current user', async() => {
           .expect(200);
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
+
+it('responds with null if not authenticated', async() => {
+  const response = 
+    await
+      request(app)
+        .get('/api/users/currentuser')
+        .send()
+        .expect(200);
+  expect(response.body.currentUser).toEqual(null);
+});
